ca27ae37e297c1442b7838c848b3d120aa160b69
Kirsten Anderson <kirsten.anderson@AV-MBP-15229.local>
2019-08-18 16:24:53 -0700

    Passes all tests

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -12,152 +12,270 @@ end
 
 # Defines the singly linked list
 class LinkedList
-    def initialize
-      @head = nil # keep the head private. Not accessible outside this class
+  def initialize
+    @head = nil # keep the head private. Not accessible outside this class
+  end
+
+  # method to add a new node with the specific data value in the linked list
+  # insert the new node at the beginning of the linked list
+  # Time Complexity: O(1), since we only access the first element in the list and don't need to traverse it
+  # Space Complexity: O(1)
+  def add_first(value)
+    node = Node.new(value, @head)
+    @head = node
+  end
+
+  # method to find if the linked list contains a node with specified value
+  # returns true if found, false otherwise
+  # Time Complexity:
+  # Space Complexity
+  def search(value)
+    raise NotImplementedError
+  end
+
+  # method to return the max value in the linked list
+  # returns the data value and not the node
+  # Time Complexity: O(n), where n is the number of nodes in the list
+  # Space Complexity O(1)
+  def find_max
+    temp_node = @head
+    if temp_node == nil
+      return nil
+    end
+    temp_max = temp_node.data
+    until temp_node == nil
+      if temp_node.data > temp_max
+        temp_max = temp_node.data
+      end
+      temp_node = temp_node.next
     end
 
-    # method to add a new node with the specific data value in the linked list
-    # insert the new node at the beginning of the linked list
-    # Time Complexity:  
-    # Space Complexity
-    def add_first(value)
-      raise NotImplementedError
+    return temp_max
+  end
+
+  # method to return the min value in the linked list
+  # returns the data value and not the node
+  # Time Complexity: O(n), where n is the number of nodes in the list
+  # Space Complexity: O(1)
+  def find_min
+    temp_node = @head
+    if temp_node == nil
+      return nil
     end
 
-    # method to find if the linked list contains a node with specified value
-    # returns true if found, false otherwise
-    # Time Complexity:  
-    # Space Complexity
-    def search(value)
-      raise NotImplementedError
+    temp_min = temp_node.data
+    until temp_node == nil
+      if temp_node.data < temp_min
+        temp_min = temp_node.data
+      end
+      temp_node = temp_node.next
     end
+    return temp_min
+  end
 
-    # method to return the max value in the linked list
-    # returns the data value and not the node
-    # Time Complexity:  
-    # Space Complexity
-    def find_max
-      raise NotImplementedError
+  # method that returns the length of the singly linked list
+  # Time Complexity: O(n), where n is the number of nodes in the list
+  # Space Complexity: O(1)
+  def length
+    counter = 0
+    temp = @head
+    if @head == nil
+      return counter
+    else
+      until (temp == nil)
+        temp = temp.next
+        counter += 1
+      end
     end
+    return counter
+  end
 
-    # method to return the min value in the linked list
-    # returns the data value and not the node
-    # Time Complexity:  
-    # Space Complexity
-    def find_min
-      raise NotImplementedError
-    end
+  # method that returns the value at a given index in the linked list
+  # index count starts at 0
+  # returns nil if there are fewer nodes in the linked list than the index value
+  # Time Complexity: O(n), where n is the number of nodes in the list
+  # Space Complexity: O(1)
+  def get_at_index(index)
+    temp_node = @head
+    counter = 0
 
-
-    # method that returns the length of the singly linked list
-    # Time Complexity:  
-    # Space Complexity
-    def length
-      raise NotImplementedError
-    end
-
-    # method that returns the value at a given index in the linked list
-    # index count starts at 0
-    # returns nil if there are fewer nodes in the linked list than the index value
-    # Time Complexity:  
-    # Space Complexity
-    def get_at_index(index)
-      raise NotImplementedError
-    end
-
-    # method to print all the values in the linked list
-    # Time Complexity:  
-    # Space Complexity
-    def visit
-      raise NotImplementedError
-    end
-
-    # method to delete the first node found with specified value
-    # Time Complexity:  
-    # Space Complexity
-    def delete(value)
-      raise NotImplementedError
-    end
-
-    # method to reverse the singly linked list
-    # note: the nodes should be moved and not just the values in the nodes
-    # Time Complexity:  
-    # Space Complexity
-    def reverse
-      raise NotImplementedError
-    end
-
-
-    ## Advanced Exercises
-    # returns the value at the middle element in the singly linked list
-    # Time Complexity:  
-    # Space Complexity
-    def find_middle_value
-      raise NotImplementedError
-    end
-
-    # find the nth node from the end and return its value
-    # assume indexing starts at 0 while counting to n
-    # Time Complexity:  
-    # Space Complexity
-    def find_nth_from_end(n)
-      raise NotImplementedError
-    end
-
-    # checks if the linked list has a cycle. A cycle exists if any node in the
-    # linked list links to a node already visited.
-    # returns true if a cycle is found, false otherwise.
-    # Time Complexity:  
-    # Space Complexity
-    def has_cycle
-      raise NotImplementedError
-    end
-
-
-    # Additional Exercises 
-    # returns the value in the first node
-    # returns nil if the list is empty
-    # Time Complexity:  
-    # Space Complexity
-    def get_first
-      raise NotImplementedError
-    end
-
-    # method that inserts a given value as a new last node in the linked list
-    # Time Complexity:  
-    # Space Complexity
-    def add_last(value)
-      raise NotImplementedError
-    end
-
-    # method that returns the value of the last node in the linked list
-    # returns nil if the linked list is empty
-    # Time Complexity:  
-    # Space Complexity
-    def get_last
-      raise NotImplementedError
-    end
-
-    # method to insert a new node with specific data value, assuming the linked
-    # list is sorted in ascending order
-    # Time Complexity:  
-    # Space Complexity
-    def insert_ascending(value)
-      raise NotImplementedError
-    end
-
-    # Helper method for tests
-    # Creates a cycle in the linked list for testing purposes
-    # Assumes the linked list has at least one node
-    def create_cycle
-      return if @head == nil # don't do anything if the linked list is empty
-
-      # navigate to last node
-      current = @head
-      while current.next != nil
-          current = current.next
+    until counter == index
+      if temp_node == nil && counter < index
+        return nil
       end
 
-      current.next = @head # make the last node link to first node
+      temp_node = temp_node.next
+      counter += 1
     end
+    return temp_node.data
+  end
+
+  # method to print all the values in the linked list
+  # Time Complexity:
+  # Space Complexity
+  def visit
+    raise NotImplementedError
+  end
+
+  # method to delete the first node found with specified value
+  # Time Complexity: O(n), where n is the number of nodes in the list
+  # Space Complexity O(1)
+  def delete(value)
+    temp_node = @head
+    return if temp_node == nil
+
+    if temp_node.data == value
+      @head = temp_node.next
+      return
+    end
+
+    until temp_node.next == nil
+      temp_next = temp_node.next
+      if temp_next.data == value
+        temp_node.next = temp_next.next
+        return
+      end
+      temp_node = temp_next
+    end
+    return
+  end
+
+  # method to reverse the singly linked list
+  # note: the nodes should be moved and not just the values in the nodes
+  # Time Complexity: O(n^2), where n is the number of nodes in the list
+  # Space Complexity: O(1)
+  def reverse
+    # find length - O(n)
+    length = self.length
+    return if length == 0
+    (length - 1).times do |i|
+      head_node = @head
+      last_node = @head
+      prev_node = @head
+      until last_node.next == nil
+        prev_node = last_node
+        last_node = last_node.next
+      end
+      if i == 0
+        temp_node = @head
+        @head = last_node
+        @head.next = temp_node
+        prev_node.next = nil
+      else
+        temp_node = @head
+        (i - 1).times do
+          temp_node = temp_node.next
+        end
+
+        last_node.next = temp_node.next
+        temp_node.next = last_node
+        prev_node.next = nil
+      end
+    end
+    return
+  end
+
+  ## Advanced Exercises
+  # returns the value at the middle element in the singly linked list
+  # Time Complexity:
+  # Space Complexity
+  def find_middle_value
+    raise NotImplementedError
+  end
+
+  # find the nth node from the end and return its value
+  # assume indexing starts at 0 while counting to n
+  # Time Complexity: O(n), where n is the number of nodes in the list
+  # Space Complexity: O(1)
+  def find_nth_from_end(n)
+    temp_node = @head
+    return nil if temp_node == nil
+    counter = 0
+    n_away = temp_node
+    until counter == n
+      return nil if n_away.next == nil
+      n_away = n_away.next
+      counter += 1
+    end
+
+    until n_away.next == nil
+      temp_node = temp_node.next
+      n_away = n_away.next
+    end
+    return temp_node.data
+  end
+
+  # checks if the linked list has a cycle. A cycle exists if any node in the
+  # linked list links to a node already visited.
+  # returns true if a cycle is found, false otherwise.
+  # Time Complexity:
+  # Space Complexity
+  def has_cycle
+    raise NotImplementedError
+  end
+
+  # Additional Exercises
+  # returns the value in the first node
+  # returns nil if the list is empty
+  # Time Complexity:
+  # Space Complexity
+  def get_first
+    node = @head
+    if node == nil
+      return node
+    end
+
+    return node.data
+  end
+
+  # method that inserts a given value as a new last node in the linked list
+  # Time Complexity:
+  # Space Complexity
+  def add_last(value)
+    temp_node = @head
+    new_node = Node.new(value)
+    if temp_node == nil
+      @head = new_node
+    else
+      until temp_node.next == nil
+        temp_node = temp_node.next
+      end
+      temp_node.next = new_node
+    end
+  end
+
+  # method that returns the value of the last node in the linked list
+  # returns nil if the linked list is empty
+  # Time Complexity:
+  # Space Complexity
+  def get_last
+    temp_node = @head
+    until temp_node.next == nil
+      temp_node = temp_node.next
+    end
+    return temp_node.data
+  end
+
+  # method to insert a new node with specific data value, assuming the linked
+  # list is sorted in ascending order
+  # Time Complexity:
+  # Space Complexity
+  def insert_ascending(value)
+    raise NotImplementedError
+  end
+
+  # Helper method for tests
+  # Creates a cycle in the linked list for testing purposes
+  # Assumes the linked list has at least one node
+  def create_cycle
+    return if @head == nil # don't do anything if the linked list is empty
+
+    # navigate to last node
+    current = @head
+    while current.next != nil
+      current = current.next
+    end
+
+    current.next = @head # make the last node link to first node
+  end
 end
--- test/linked_list_test.rb
+++ test/linked_list_test.rb
@@ -1,236 +1,235 @@
-require 'minitest/autorun'
-require 'minitest/reporters'
-require 'minitest/skip_dsl'
+require "minitest/autorun"
+require "minitest/reporters"
+require "minitest/skip_dsl"
 
-require_relative 'test_helper'
-
+require_relative "test_helper"
 
 Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 
 describe LinkedList do
-    # Arrange
-    before do
-        @list = LinkedList.new
+  # Arrange
+  before do
+    @list = LinkedList.new
+  end
+
+  describe "initialize" do
+    it "can be created" do
+
+      # Assert
+      expect(@list).must_be_kind_of LinkedList
+    end
+  end
+
+  describe "add_first & get_first" do
+    it "can add values to an empty list" do
+      # Act
+      @list.add_first(3)
+
+      # Assert
+      expect(@list.get_first).must_equal 3
     end
 
-    describe 'initialize' do
-        it 'can be created' do
+    it "will put the last added item to the front of the list" do
+      # Act
+      @list.add_first(1)
+      @list.add_first(2)
 
-            # Assert
-            expect(@list).must_be_kind_of LinkedList
-        end
+      # Assert
+      expect(@list.get_first).must_equal 2
+
+      # Act again
+      @list.add_first(3)
+
+      # Assert
+      expect(@list.get_first).must_equal 3
     end
 
-    describe 'add_first & get_first' do
-        it 'can add values to an empty list' do
-            # Act
-            @list.add_first(3)
+    it "will return `nil` for `getFirst` if the list is empty" do
+      # Act-Assert
+      expect(@list.get_first).must_be_nil
+    end
+  end
 
-            # Assert
-            expect(@list.get_first).must_equal 3
-        end
-
-        it 'will put the last added item to the front of the list' do
-            # Act
-            @list.add_first(1)
-            @list.add_first(2)
-
-            # Assert
-            expect(@list.get_first).must_equal 2
-
-            # Act again
-            @list.add_first(3)
-
-            # Assert
-            expect(@list.get_first).must_equal 3
-        end
-
-        it 'will return `nil` for `getFirst` if the list is empty' do
-            # Act-Assert
-            expect(@list.get_first).must_be_nil
-        end
+  describe "length" do
+    it "will return 0 for an empty list" do
+      # Act-Assert
+      expect(@list.length).must_equal 0
     end
 
-    describe "length" do
-        it "will return 0 for an empty list" do
-            # Act-Assert
-            expect(@list.length).must_equal 0
-        end
+    it "will return the length for nonempty lists" do
+      # Arrange
+      count = 0
+      while count < 5
+        @list.add_first(count)
+        count += 1
+        # Act-Assert
+        expect(@list.length).must_equal count
+      end
+    end
+  end
 
-        it "will return the length for nonempty lists" do
-            # Arrange
-            count = 0
-            while count < 5
-                @list.add_first(count)
-                count += 1
-                # Act-Assert
-                expect(@list.length).must_equal count
-            end
-        end
+  describe "addLast & getLast" do
+    it "will add to the front if the list is empty" do
+      # Arrange
+      @list.add_last(1)
+      # Act-Assert
+      expect(@list.get_first).must_equal 1
     end
 
-    describe "addLast & getLast" do
-        it "will add to the front if the list is empty" do
-            # Arrange
-            @list.add_last(1)
-            # Act-Assert
-            expect(@list.get_first).must_equal 1
-        end
+    it "will put new items to the rear of the list" do
+      # Arrange
+      @list.add_last(2)
+      # Act-Assert
+      expect(@list.length).must_equal 1
+      expect(@list.get_last).must_equal 2
 
-        it "will put new items to the rear of the list" do
-            # Arrange
-            @list.add_last(2)
-            # Act-Assert
-            expect(@list.length).must_equal 1
-            expect(@list.get_last).must_equal 2
+      # Arrange
+      @list.add_last(3)
+      # Act-Assert
+      expect(@list.get_first).must_equal 2
+      expect(@list.get_last).must_equal 3
+      expect(@list.length).must_equal 2
 
-            # Arrange
-            @list.add_last(3)
-            # Act-Assert
-            expect(@list.get_first).must_equal 2
-            expect(@list.get_last).must_equal 3
-            expect(@list.length).must_equal 2
+      @list.add_last(4)
+      expect(@list.get_first).must_equal 2
+      expect(@list.get_last).must_equal 4
+      expect(@list.length).must_equal 3
+    end
+  end
 
-            @list.add_last(4)
-            expect(@list.get_first).must_equal 2
-            expect(@list.get_last).must_equal 4
-            expect(@list.length).must_equal 3
-        end
+  describe "get_at_index" do
+    it "returns nil if the index is outside the bounds of the list" do
+      # Act-Assert
+      expect(@list.get_at_index(3)).must_be_nil
     end
 
-    describe 'get_at_index' do
-        it 'returns nil if the index is outside the bounds of the list' do
-            # Act-Assert
-            expect(@list.get_at_index(3)).must_be_nil
-        end
+    it "can retrieve an item at an index in the list" do
+      # Arrange
+      @list.add_first(1)
+      @list.add_first(2)
+      @list.add_first(3)
+      @list.add_first(4)
 
-        it 'can retrieve an item at an index in the list' do
-            # Arrange
-            @list.add_first(1)
-            @list.add_first(2)
-            @list.add_first(3)
-            @list.add_first(4)
+      # Act-Assert
+      expect(@list.get_at_index(0)).must_equal 4
+      expect(@list.get_at_index(1)).must_equal 3
+      expect(@list.get_at_index(2)).must_equal 2
+      expect(@list.get_at_index(3)).must_equal 1
+    end
+  end
 
-            # Act-Assert
-            expect(@list.get_at_index(0)).must_equal 4
-            expect(@list.get_at_index(1)).must_equal 3
-            expect(@list.get_at_index(2)).must_equal 2
-            expect(@list.get_at_index(3)).must_equal 1
-        end
+  describe "max and min values" do
+    it "returns nil if the list is empty" do
+      # Act-Assert
+      expect(@list.find_max()).must_be_nil
+      expect(@list.find_min()).must_be_nil
     end
 
-    describe 'max and min values' do
-        it 'returns nil if the list is empty' do
-            # Act-Assert
-            expect(@list.find_max()).must_be_nil
-            expect(@list.find_min()).must_be_nil
-        end
+    it "can retrieve the max and min values in the list" do
+      # Arrange
+      count = 0
+      while count < 5
+        @list.add_first(count)
+        expect(@list.find_max).must_equal count
+        expect(@list.find_min).must_equal 0
+        count += 1
+      end
+      @list.add_last(100)
+      @list.add_first(-12)
 
-        it 'can retrieve the max and min values in the list' do
-            # Arrange
-            count = 0
-            while count < 5
-                @list.add_first(count)
-                expect(@list.find_max).must_equal count
-                expect(@list.find_min).must_equal 0
-                count += 1
-            end
-            @list.add_last(100)
-            @list.add_first(-12)
-            
-            # Act-Assert
-            expect(@list.find_max).must_equal 100
-            expect(@list.find_min).must_equal(-12)
-        end
+      # Act-Assert
+      expect(@list.find_max).must_equal 100
+      expect(@list.find_min).must_equal(-12)
+    end
+  end
+
+  describe "delete" do
+    it "delete from empty linked list is a no-op" do
+      # Assert
+      expect(@list.length).must_equal 0
+      # Act
+      @list.delete(4)
+      # Assert
+      expect(@list.length).must_equal 0
     end
 
-    describe "delete" do
-        it "delete from empty linked list is a no-op" do
-            # Assert
-            expect(@list.length).must_equal 0
-            # Act
-            @list.delete(4)
-            # Assert
-            expect(@list.length).must_equal 0
-        end
+    it "can delete valid values from list" do
+      # Arrange
+      @list.add_last(9)
+      @list.add_last(10)
+      @list.add_first(4)
+      @list.add_first(3)
+      @list.add_first(2)
 
-        it "can delete valid values from list" do
-            # Arrange
-            @list.add_last(9)
-            @list.add_last(10)
-            @list.add_first(4)
-            @list.add_first(3)
-            @list.add_first(2)
-            
-            # Act
-            # delete fist node (requires updating head)
-            @list.delete(2)
-            
-            # Assert
-            expect(@list.get_first).must_equal 3
-            expect(@list.length).must_equal 4
-            expect(@list.get_last).must_equal 10
-            expect(@list.find_max).must_equal 10
-            expect(@list.find_min).must_equal 3
-            
-            # Act (again)
-            # delete last node
-            @list.delete(10)
-            # Assert
-            expect(@list.get_first).must_equal 3
-            expect(@list.length).must_equal 3
-            expect(@list.get_last).must_equal 9
-            expect(@list.find_max).must_equal 9
-            expect(@list.find_min).must_equal 3
+      # Act
+      # delete fist node (requires updating head)
+      @list.delete(2)
 
-            # delete fist node (requires updating head)
-            @list.delete(4)
-            expect(@list.get_first).must_equal 3
-            expect(@list.length).must_equal 2
-            expect(@list.get_last).must_equal 9
-            expect(@list.find_max).must_equal 9
-            expect(@list.find_min).must_equal 3
-        end
+      # Assert
+      expect(@list.get_first).must_equal 3
+      expect(@list.length).must_equal 4
+      expect(@list.get_last).must_equal 10
+      expect(@list.find_max).must_equal 10
+      expect(@list.find_min).must_equal 3
+
+      # Act (again)
+      # delete last node
+      @list.delete(10)
+      # Assert
+      expect(@list.get_first).must_equal 3
+      expect(@list.length).must_equal 3
+      expect(@list.get_last).must_equal 9
+      expect(@list.find_max).must_equal 9
+      expect(@list.find_min).must_equal 3
+
+      # delete fist node (requires updating head)
+      @list.delete(4)
+      expect(@list.get_first).must_equal 3
+      expect(@list.length).must_equal 2
+      expect(@list.get_last).must_equal 9
+      expect(@list.find_max).must_equal 9
+      expect(@list.find_min).must_equal 3
+    end
+  end
+
+  describe "nth_from_the_end" do
+    it "returns nil if n is outside the bounds of the list" do
+      # Act-Assert
+      expect(@list.find_nth_from_end(3)).must_be_nil
     end
 
-    describe "nth_from_the_end" do
-        it 'returns nil if n is outside the bounds of the list' do
-            # Act-Assert
-            expect(@list.find_nth_from_end(3)).must_be_nil
-        end
+    it "can retrieve an item at index n from the end in the list" do
+      # Arrange
+      @list.add_first(1)
+      @list.add_first(2)
+      @list.add_first(3)
+      @list.add_first(4)
 
-        it 'can retrieve an item at index n from the end in the list' do
-            # Arrange
-            @list.add_first(1)
-            @list.add_first(2)
-            @list.add_first(3)
-            @list.add_first(4)
-
-            # Act-Assert
-            expect(@list.find_nth_from_end(0)).must_equal 1
-            expect(@list.find_nth_from_end(1)).must_equal 2
-            expect(@list.find_nth_from_end(2)).must_equal 3
-            expect(@list.find_nth_from_end(3)).must_equal 4
-            expect(@list.find_nth_from_end(4)).must_be_nil
-        end
+      # Act-Assert
+      expect(@list.find_nth_from_end(0)).must_equal 1
+      expect(@list.find_nth_from_end(1)).must_equal 2
+      expect(@list.find_nth_from_end(2)).must_equal 3
+      expect(@list.find_nth_from_end(3)).must_equal 4
+      expect(@list.find_nth_from_end(4)).must_be_nil
     end
+  end
 
-    describe "reverse" do
-        it 'can retrieve an item at index n from the end in the list' do
-            # Arrange
-            @list.add_first(4)
-            @list.add_first(3)
-            @list.add_first(2)
-            @list.add_first(1)
-            
-            # Act
-            @list.reverse
+  describe "reverse" do
+    it "can retrieve an item at index n from the end in the list" do
+      # Arrange
+      @list.add_first(4)
+      @list.add_first(3)
+      @list.add_first(2)
+      @list.add_first(1)
 
-            # Assert
-            expect(@list.find_nth_from_end(0)).must_equal 1
-            expect(@list.find_nth_from_end(1)).must_equal 2
-            expect(@list.find_nth_from_end(2)).must_equal 3
-            expect(@list.find_nth_from_end(3)).must_equal 4
-        end
+      # Act
+      @list.reverse
+
+      # Assert
+      expect(@list.find_nth_from_end(0)).must_equal 1
+      expect(@list.find_nth_from_end(1)).must_equal 2
+      expect(@list.find_nth_from_end(2)).must_equal 3
+      expect(@list.find_nth_from_end(3)).must_equal 4
     end
+  end
 end
